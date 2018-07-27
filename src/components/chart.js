0999a2defcff48999d86ac524e8f8249559c1ac4
travisdock <travisdock@gmail.com>
2018-07-27 17:15:17 -0400

    fix chart cycle

--- src/components/chart.js
+++ src/components/chart.js
@@ -6,7 +6,8 @@ class Chart extends React.Component {
 
   state = {
     columns: [],
-    load: false
+    load: false,
+    currentChart: 0
   }
   componentDidMount() {
     const id = this.props.current_user.user_id
@@ -16,20 +17,24 @@ class Chart extends React.Component {
       .then(resp => this.setState({columns: resp, load: true }, this.renderChart))
   }
 
+  changeChart = () => {
+    const max = this.state.columns.length
+    this.setState((prevState) => {
+      return {
+        currentChart: (prevState.currentChart === (max - 1) ? 0 : prevState.currentChart + 1)
+      }
+    }, this.renderChart)
+  }
   renderChart() {
+    console.log(this.state)
+    let month = Object.keys(this.state.columns[this.state.currentChart])[0]
     bb.generate({
       data: {
-        x: "x",
-        columns: this.state.columns,
-    type: "bar"
+        columns: this.state.columns[this.state.currentChart][month],
+        type: "pie"
       },
-      axis: {
-        x: {
-          type: "timeseries",
-          tick: {
-            format: "%b"
-          }
-        }
+      title: {
+        text: month
       },
       bindto: "#chart"
     });
@@ -39,10 +44,14 @@ class Chart extends React.Component {
     console.log(this.state)
     return (
       <div>
-        {this.state.load ? <div id="chart" /> : null}
-
+        {this.state.load ?
+            <div>
+              <div id="chart" />
+              <button onClick={this.changeChart}>Next</button>
+            </div>
+          : null
+        }
       </div>
-
     )
   }
 }
@@ -51,3 +60,20 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps)(Chart)
+
+
+
+// WORKING BAR CHART
+// data: {
+//   x: "x",
+//   columns: this.state.columns,
+// type: "bar"
+// },
+// axis: {
+//   x: {
+//     type: "timeseries",
+//     tick: {
+//       format: "%b"
+//     }
+//   }
+// }
