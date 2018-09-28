d38fe0a56d3b3c9273d2f6aab5ac88d9e20850d9
Travis <travisdock@gmail.com>
2018-09-28 07:03:58 -0400

    update format money function

--- src/components/table.js
+++ src/components/table.js
@@ -81,7 +81,7 @@ updateWindowDimensions() {
 
 ////////////////////////////////////////////////////////////////
 
-////////unfinished manipulate filtered data fucntions///////////
+///////////////manipulate filtered data functions//////////////
   formatMoney = (number, places, symbol, thousand, decimal) => {
     number = number || 0;
     places = !isNaN(places = Math.abs(places)) ? places : 2;
@@ -90,7 +90,7 @@ updateWindowDimensions() {
     decimal = decimal || ".";
     var negative = number < 0 ? "-" : "",
         i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
-        j = (j = i.length) > 3 ? j % 3 : 0;
+        j = i.length > 3 ? i.length % 3 : 0;
     return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
   }
 
