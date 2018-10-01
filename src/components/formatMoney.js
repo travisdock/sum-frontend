5a2d59ee878136c6335eabd8f75a2cdbede01e8b
Travis <travisdock@gmail.com>
2018-10-01 07:44:58 -0400

    add string handling to formatMoney function

--- src/components/formatMoney.js
+++ src/components/formatMoney.js
@@ -2,7 +2,10 @@
 //I took this function from ^this gist but changed the j declaration
 //we will see if it breaks
 export const formatMoney = (number, places, symbol, thousand, decimal) => {
-    number = number || 0;
+    if (number === "not enough data") {
+        return "N/A"
+    } else {
+        number = number || 0;
     places = !isNaN(places = Math.abs(places)) ? places : 2;
     symbol = symbol !== undefined ? symbol : "$";
     thousand = thousand || ",";
@@ -11,4 +14,5 @@ export const formatMoney = (number, places, symbol, thousand, decimal) => {
         i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
         j = i.length > 3 ? i.length % 3 : 0;
     return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+    }
   }
