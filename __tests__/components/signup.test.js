5cd668c190e6d252e0e6813e99cbf72298eb90fe
Travis <travisdock@gmail.com>
2018-10-25 06:41:49 -0400

    abandon App.js test, got a lot of signup.test.js working

--- __tests__/components/signup.test.js
+++ __tests__/components/signup.test.js
@@ -4,34 +4,37 @@ import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store'; // Smart components
 
 // Component to be tested
-import { Signup } from '../../src/components/signup';
+import { noRouterSignUp as Signup } from '../../src/components/signup';
 
 // Setup Store
 const mockStore = configureStore();
-const initialState = {current_user: { user_id: 1 }}
+const initialState = {current_user: {}}
 const store = mockStore(initialState);
 
 
 describe('<SignUp />', () => {
     describe('render()', () => {
-            test('renders the component', () => {
-                const wrapper = shallow(<Signup store={store}/>);
-                const component = wrapper.dive();
+        test('renders the component', () => {
+            const wrapper = shallow(<Signup store={store}/>);
+            const component = wrapper.dive();
 
-                expect(toJson(component)).toMatchSnapshot();
+            expect(toJson(component)).toMatchSnapshot();
         });
     });
-    // describe('onClick() signup button', () => {
-    //     test('successfully calls the onClick handler', () => {
-    //         const mockOnClick = jest.fn();
-    //         const wrapper = shallow(
-    //             <Signup onClick={mockOnClick} label="Eat Food" />
-    //         );
-    //         const component = wrapper.dive();
+    describe('onClick() signup button', () => {
+        test('successfully calls the onClick handler', () => {
+            const mockOnClick = jest.fn();
+            const wrapper = shallow(
+                <Signup handleSubmit={mockOnClick} store={store}/>
+            );
+            const component = wrapper.dive();
+            const signUpButton = component.find('button')
+            // signUpButton.simulate('click');
+            const form = component.find('form')
+            form.simulate('submit')
 
-    //         component.find('button').simulate('click');
-
-    //         expect(mockOnClick.mock.calls.length).toEqual(1);
-    //     });
-    // });
+            expect(signUpButton.text()).toEqual('Sign up')
+            expect(mockOnClick.mock.calls.length).toEqual(1);
+        });
+    });
 });
