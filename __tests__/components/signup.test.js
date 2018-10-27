e60e249dd0116c1ab6f2aef175b95ed74dc4c834
Travis <travisdock@gmail.com>
2018-10-27 11:23:25 -0400

    all signup tests are working with shallow rendering now

--- __tests__/components/signup.test.js
+++ __tests__/components/signup.test.js
@@ -4,14 +4,17 @@ import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store'; // Smart components
 
 // Component to be tested
-import { noRouterSignUp as Signup, nakedSignup as NakedSignup } from '../../src/components/signup';
+import {
+    noRouterSignUp as ReduxWrappedSignup,
+    nakedSignup as NakedSignup
+} from '../../src/components/signup';
 
 // Setup Store
 const mockStore = configureStore();
 const initialState = {current_user: {}}
 const store = mockStore(initialState);
 
-// Mock fetch
+// Mock fetches
 function mockFetch(data) {
     return jest.fn().mockImplementation(() =>
       Promise.resolve({
@@ -20,37 +23,109 @@ function mockFetch(data) {
       })
     );
   }
+const mockResponse = (status, statusText, response) => {
+    return new Response(response, {
+        status: status,
+        statusText: statusText,
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+};
 
+describe('<SignUp /> with redux', () => {
+    it('renders the component', () => {
+        const wrapper = shallow(<ReduxWrappedSignup store={store}/>);
+        const component = wrapper.dive();
 
-describe('<SignUp />', () => {
-    describe('render()', () => {
-        test('renders the component', () => {
-            const wrapper = shallow(<Signup store={store}/>);
-            const component = wrapper.dive();
-
-            expect(toJson(component)).toMatchSnapshot();
-        });
+        expect(toJson(component)).toMatchSnapshot();
     });
 });
 
 describe('<Signup /> methods, without wrappers', () => {
+
     it('handleSubmit is fired on button click', () => {
         const spy = jest.spyOn(NakedSignup.prototype, "handleSubmit");
-        const props = {user_id: null}
         const fetch = mockFetch({"cool": "yeah"})
-        const component = mount(
+        const props = {user_id: null}
+
+        const component = shallow(
             <NakedSignup current_user={ props }/>
         );
+
         const signUpButton = component.find('button')
-        signUpButton.simulate('submit');
+        const form = component.find('form')
+        form.simulate('submit', { preventDefault () {} });
 
         expect(signUpButton.text()).toEqual('Sign up')
         expect(spy).toHaveBeenCalled();
     });
-});
 
-// I cannot spy on instance methods because of the redux wrapped component.
-// I have to figure out how to do this without redux unfortunately...
-// Ok, make different tests for each thing. Like a test for only instance methods
-//          will (probably?) not need redux or router so don't import them...
-//          So only import them if you are testing them specifically..
+    it('responds with an error alert if errors on fetch', async (done) => {
+        window.alert = jest.fn()
+        const props = {user_id: null}
+
+        const fakePromise = Promise.resolve(mockResponse(
+            200,
+            null,
+           JSON.stringify({errors: "invalid username or password"})
+        ));
+        window.fetch = jest.fn().mockImplementationOnce(
+            () => {
+                return fakePromise
+            }
+        );
+        expect.assertions(1);
+
+        const component = shallow(
+            <NakedSignup current_user={ props }/>
+        );
+
+        const form = component.find('form')
+        await form.simulate('submit', { preventDefault () {} });
+
+        setImmediate(() => {
+            try {
+                expect(window.alert).toHaveBeenCalledWith('invalid username or password');
+            } catch (e) {
+                done.fail(e);
+            }
+            done();
+        });
+    });
+
+    it('responds with sucess message if fetch response has no errors', async (done) => {
+        window.alert = jest.fn()
+        const user = {user_id: null}
+        const history = {push: jest.fn() }
+
+        const fakePromise = Promise.resolve(mockResponse(
+            200,
+            null,
+           JSON.stringify({user: "user info here"})
+        ));
+        window.fetch = jest.fn().mockImplementationOnce(
+            () => {
+                return fakePromise
+            }
+        );
+        expect.assertions(1);
+
+        const component = shallow(
+            <NakedSignup current_user={user} history={history} />
+        );
+
+        const form = component.find('form')
+        await form.simulate('submit', { preventDefault () {} });
+
+        setImmediate(() => {
+            try {
+                expect(window.alert).toHaveBeenCalledWith('Success! Please log in.');
+            } catch (e) {
+                done.fail(e);
+            }
+            done();
+        });
+    });
+    
+});
