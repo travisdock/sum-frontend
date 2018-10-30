64794f6d323ea7dc206e6443111322c9abaa916a
Travis <travisdock@gmail.com>
2018-10-29 20:18:54 -0400

    moreMenu works now

--- src/components/moreMenu.js
+++ src/components/moreMenu.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import Popup from "reactjs-popup";
+import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { logout } from '../actions/index'
-
-import { withRouter } from 'react-router';
 
 class MoreMenu extends React.Component {
     constructor() {
         super();
         this.handleLogout = this.handleLogout.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.openModal = this.openModal.bind(this);
+    }
+    state = {
+        open: false
     }
 
+    closeModal() {
+        this.setState({ open: false })
+    };
+    openModal() {
+        this.setState({ open: true })
+    };
     handleLogout() {
         localStorage.clear()
         this.props.logout()
         this.props.history.push('/login')
-    }
+    };
 
     render() {
         return (
-            <Popup
-                trigger={<button className="more"> More </button>}
+            <div>
+                <button className="more" onClick={this.openModal}> More </button>
+                <Popup
                 modal
+                open={this.state.open}
+                onClose={this.closeModal}
                 closeOnDocumentClick
                 >
                     <div className="navbar-popup">
                         <NavLink
                             to='/dashboard/csv'
+                            onClick={this.closeModal}
                         >Import CSV</NavLink>
                         <br></br>
                         <button
@@ -36,6 +50,7 @@ class MoreMenu extends React.Component {
                         >Logout</button>
                     </div>
                 </Popup>
+            </div>
         )
     }
 }
@@ -43,6 +58,5 @@ class MoreMenu extends React.Component {
 const mapStateToProps = state => ({
     current_user: state.current_user
   });
-  
-  export default withRouter(connect(mapStateToProps, { logout })(MoreMenu))
+export default withRouter(connect(mapStateToProps, { logout })(MoreMenu))
   
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { logout } from '../actions/index'
-
 import { withRouter } from 'react-router';
-
 import MoreMenu from './moreMenu.js';
 
 class Navbar extends React.Component {
@@ -47,5 +44,4 @@ class Navbar extends React.Component {
 const mapStateToProps = state => ({
   current_user: state.current_user
 });
-
-export default withRouter(connect(mapStateToProps, { logout })(Navbar))
+export default withRouter(connect(mapStateToProps)(Navbar))
