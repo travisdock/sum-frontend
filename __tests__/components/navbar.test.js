999c76ec53d07a75ba5863366a47cbf35250fe14
Travis <travisdock@gmail.com>
2018-10-30 20:55:56 -0400

    fix navbar tests to take current user into account

--- __tests__/components/navbar.test.js
+++ __tests__/components/navbar.test.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { Provider } from "react-redux";
 import configureStore from 'redux-mock-store'; // Smart components
-import { MemoryRouter, NavLink } from 'react-router';
+import { MemoryRouter } from 'react-router';
 
 import Navbar from '../../src/components/navbar';
 
 // Setup Store
 const mockStore = configureStore();
-const initialState = {current_user: {}}
-const store = mockStore(initialState);
+const userState = {current_user: {user_id: 1}}
+const userStore = mockStore(userState);
+const noUserState = {current_user: {}};
+const noUserStore = mockStore(noUserState);
 
-// props for no redux
-const userProp = {user_id: null}
-const historyProp = {push: jest.fn()}
+describe('<Navbar />', () => {
+    it('given a user, it renders the component correctly', () => {
+        const wrapper = mount(
+            <Provider store={userStore}>
+                <MemoryRouter 
+                // this fakes the key in history to be testKey every time so it doesn't mess
+                // up the snapshot.
+                // https://github.com/ReactTraining/react-router/issues/5579#issuecomment-333401692
+                initialEntries={[ { pathname: '/', key: 'testKey' } ]}
+                >
+                    <Navbar store={userStore} />
+                </MemoryRouter>
+            </Provider>
+        );
 
-describe('Navbar', () => {
-    it('renders the component', () => {
+        expect(toJson(wrapper)).toMatchSnapshot();
+        expect(wrapper.find('NavLink')).toHaveLength(3);
+        expect(wrapper.find('NavLink').get(0).props.to).toBe('/dashboard/form');
+        expect(wrapper.find('NavLink').get(1).props.to).toBe('/dashboard/charts');
+        expect(wrapper.find('NavLink').get(2).props.to).toBe('/dashboard/entries');
+    });
+
+    it('given no user, it renders the component correctly', () => {
         const wrapper = mount(
             <MemoryRouter 
               // this fakes the key in history to be testKey every time so it doesn't mess
@@ -24,12 +44,16 @@ describe('Navbar', () => {
               // https://github.com/ReactTraining/react-router/issues/5579#issuecomment-333401692
               initialEntries={[ { pathname: '/', key: 'testKey' } ]}
             >
-                <Navbar store={store} current_user={userProp} />
+                <Navbar store={noUserStore} />
             </MemoryRouter>
         );
 
 
         expect(toJson(wrapper)).toMatchSnapshot();
-        // expect(wrapper.find(NavLink).props().to).toBe('/login');
-    });
+        expect(wrapper.find('NavLink')).toHaveLength(3);
+        expect(wrapper.find('NavLink').get(0).props.to).toBe('/login');
+        expect(wrapper.find('NavLink').get(1).props.to).toBe('/about');
+        expect(wrapper.find('NavLink').get(2).props.to).toBe('/signup');
+    }); 
+        
 });
