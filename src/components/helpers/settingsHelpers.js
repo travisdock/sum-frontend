9fc7eac310a2a5851df802ca0964f7203df49299
Travis <travisdock@gmail.com>
2018-12-20 14:57:05 -0700

    fix auth

--- src/components/helpers/settingsHelpers.js
+++ src/components/helpers/settingsHelpers.js
@@ -30,7 +30,6 @@ export function handleUserUpdate(e) {
     const token = localStorage.getItem('jwt')
     const element = e.target.form.elements[0]
     const user_id = this.props.current_user.user_id
-    const token = localStorage.getItem('jwt')
     const body = {
         user_id: user_id,
         [element.name]: parseInt(element.value)
@@ -41,9 +40,6 @@ export function handleUserUpdate(e) {
           "Content-Type": "application/json",
           "Accept": "application/json",
           'Authorization': token
-        },
-        headers: {
-            'Authorization': token
         },
         body: JSON.stringify(body)
     };
