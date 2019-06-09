54a5c6fb6573471b5a980af213928fa536157c92
Travis <travisdock@gmail.com>
2019-06-09 15:48:17 -0500

    Par down mocks and fix typos

--- src/components/__mocks__/Login.js
+++ src/components/__mocks__/Login.js
@@ -1,59 +1,16 @@
 import React from 'react';
 
 export default class Login extends React.Component {
-  state = {
-    username: '',
-    password: ''
-  };
-
-  handleChange = (e) => {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(this.state)
-    };
-    fetch(`${process.env.REACT_APP_API}/api/v1/login`, options)
-    .then(resp => resp.json())
-    .then(resp => {
-      if (resp.error) {
-        alert(resp.error)
-        this.setState({
-          username: '',
-          password: ''
-        })
-      } else {
-        localStorage.setItem('jwt', resp.jwt);
-        this.props.login(resp);
-      }
-    })
-  }
-
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <h1 className="logo">Sum.</h1>
           <div className="ui field">
             <input
               name="username"
               placeholder="Username"
               type="text"
-              value={this.state.username}
-              onChange={this.handleChange}
             />
           </div>
           <div className="ui field">
@@ -61,8 +18,6 @@ export default class Login extends React.Component {
               name="password"
               placeholder="Password"
               type="password"
-              value={this.state.password}
-              onChange={this.handleChange}
             />
           </div>
           <button type="submit" className="button">
@@ -73,4 +28,3 @@ export default class Login extends React.Component {
     )
   }
 }
-
--- src/components/__mocks__/Signup.js
+++ src/components/__mocks__/Signup.js
@@ -1,61 +1,15 @@
 import React from 'react';
 
-export  default class Signup extends React.Component {
-  constructor() {
-    super()
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-  state = {
-    username: '',
-    password: '',
-    email: ''
-  };
-
-  handleChange(e) {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const url = `${process.env.REACT_APP_API}/api/v1/users`
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(this.state)
-    }
-    fetch(url, options)
-      .then(res => res.json())
-      .then(resp => {
-        if (resp.errors) {
-          alert(resp.errors)
-        } else {
-          alert("Success! Please log in.")
-          this.props.history.push('/login')
-        }
-      })
-  }
-
+export default class Signup extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <h1 className="logo">Sum.</h1>
           <div className="ui field">
             <input
               name="username"
               placeholder="Username..."
-              value={this.state.username}
-              onChange={this.handleChange}
             />
           </div>
           <div className="ui field">
@@ -63,16 +17,12 @@ export  default class Signup extends React.Component {
               name="password"
               type="password"
               placeholder="Password..."
-              value={this.state.password}
-              onChange={this.handleChange}
             />
           </div>
           <div className="ui field">
             <input
               name="email"
               placeholder="Email..."
-              value={this.state.email}
-              onChange={this.handleChange}
             />
           </div>
           <p>(this does not have to be a real email)</p>
